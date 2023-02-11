fd0bbe5ea1b7102da0f1393f5b18b7a8e6589af4
S1n1ster <124812647+H1gggs@users.noreply.github.com>
2023-02-11 15:40:41 +0400

    Update SpreadSheet.cpp

--- SpreadSheet/SpreadSheet.cpp
+++ SpreadSheet/SpreadSheet.cpp
@@ -1,21 +1,11 @@
 #include <iostream>
 #include "SpreadSheet.h"
 SpreadSheet::SpreadSheet() {}
-SpreadSheet::~SpreadSheet() {
-	for (int i = 0; i < Row; ++i) {
-		delete[] Cells[i];
-		Cells[i] = nullptr;
-	}
-	delete[] Cells;
-	Cells = nullptr;
-}
+SpreadSheet::~SpreadSheet() {}
 SpreadSheet::SpreadSheet(int Row, int Col) {
 	this->Row = Row;
 	this->Col = Col;
-	Cells = new Cell * [Col];
-	for (int i = 0; i < Row; ++i) {
-		Cells[i] = new Cell[Col];			
-	}
+	Cells.resize(Row, std::vector<Cell>(Col));
 	for (int i = 0; i < Row; ++i) {
 		for (int j = 0; j < Col; ++j) {
 			Cells[i][j].m_value = "";
@@ -23,26 +13,54 @@ SpreadSheet::SpreadSheet(int Row, int Col) {
 		}
 	}
 }
-void SpreadSheet::SetCellAt(int row, int col, const Cell& cl) {  
+void SpreadSheet::SetCellAt(int row, int col, const Cell& cl) { 
+	if ((row >= Row) || (col >= Col)) {
+		std::cout << "invalid data" << std::endl;
+		return;
+	}
+	if ((row < 0) || (col < 0)) {
+		std::cout << "invalid data" << std::endl;
+		return;
+	}
 	Cells[row][col].m_color = cl.m_color;
 	Cells[row][col].m_value = cl.m_value;
 }
 void SpreadSheet::SetCellAt(int row, int col,const std::string& str) { 
+	if ((row >= Row) || (col >= Col)) {
+		std::cout << "invalid data" << std::endl;
+		return;
+	}
+	if ((row < 0) || (col < 0)) {
+		std::cout << "invalid data" << std::endl;
+		return;
+	}
 	Cells[row][col].m_value = str;
 	Cells[row][col].m_color = color::no_color;
 }
 Cell SpreadSheet::GetCellAt(int row, int col) { 
-	
+	if ((row >= Row) || (col >= Col)) {
+		std::cout << "invalid data" << std::endl;
+		return Cells[-1][-1];
+	}
+	if ((row < 0) || (col < 0)) {
+		std::cout << "invalid data" << std::endl;
+		return Cells[-1][-1];
+	}
 	return Cells[row][col];
 
 }
 void SpreadSheet::AddRow(int row) {
-	int Tmp_Row = Row + 1;
-	Cell** Tmp_Cells;
-	Tmp_Cells = new Cell * [Col];
-	for (int i = 0; i < Tmp_Row; ++i) {
-		Tmp_Cells[i] = new Cell[Col];		
+	if (row >= Row) {
+		std::cout << "invalid data" << std::endl;
+		return;
 	}
+	if (row < 0) {
+		std::cout << "invalid data" << std::endl;
+		return;
+	}
+	int Tmp_Row = Row + 1;
+	std::vector<std::vector<Cell>> Tmp_Cells;
+	Tmp_Cells.resize(Tmp_Row, std::vector<Cell>(Col));
 	for (int i = 0; i < Tmp_Row; ++i) {
 		for (int j = 0; j < Col; ++j) {
 			Tmp_Cells[i][j].m_value = "";
@@ -53,46 +71,31 @@ void SpreadSheet::AddRow(int row) {
 
 	for (; k < row; ++k) {
 		for (int i = 0; i < Col; ++i) {
-			Tmp_Cells[k][i].m_value = Cells[k][i].m_value;
-			Tmp_Cells[k][i].m_color = Cells[k][i].m_color;
+			Tmp_Cells[k][i].m_value = this->Cells[k][i].m_value;
+			Tmp_Cells[k][i].m_color = this->Cells[k][i].m_color;
 		}
 	}
 	for (int i = row + 1; i < Tmp_Row; ++i) {
 		for (int j = 0; j < Col; ++j) {
-			Tmp_Cells[i][j].m_value = Cells[k][j].m_value;
-			Tmp_Cells[i][j].m_color = Cells[k][j].m_color;
+			Tmp_Cells[i][j].m_value = this->Cells[k][j].m_value;
+			Tmp_Cells[i][j].m_color = this->Cells[k][j].m_color;
 		}
 		k++;
 	}
-	for (int i = 0; i < Row; ++i) {
-		delete[] Cells[i];
-	}
-	delete[] Cells;
 	Row++;
-	Cells = new Cell * [Col];
-	for (int i = 0; i < Row; ++i) {
-		Cells[i] = new Cell[Col];
-	}
+	Cells.resize(Row, std::vector<Cell>(Col));
 	for (int i = 0; i < Row; ++i) {
 		for (int j = 0; j < Col; ++j) {
-			Cells[i][j].m_value = Tmp_Cells[i][j].m_value;
-			Cells[i][j].m_color = Tmp_Cells[i][j].m_color;
+			this->Cells[i][j].m_value = Tmp_Cells[i][j].m_value;
+			this->Cells[i][j].m_color = Tmp_Cells[i][j].m_color;
 		}
 	}
-	for (int i = 0; i < Tmp_Row; ++i) {
-		delete[] Tmp_Cells[i];
-	}
-	delete[] Tmp_Cells;
-
 }
 void SpreadSheet::RemoveRow(int row) {
-	Cell** newCells = new Cell * [Row-1];
-	for (int i = 0; i < Row-1; i++)
-	{
-		newCells[i] = new Cell[Col];
-	}
-
-	for (int i = 0,newRoW = 0; i < Row; i++)
+	int Tmp_Row = Row - 1;
+	std::vector<std::vector<Cell>> Tmp_Cells;
+	Tmp_Cells.resize(Tmp_Row, std::vector<Cell>(Col));
+	for (int i = 0,index = 0; i < Row; i++)
 	{
 		if (i == row)
 		{
@@ -100,109 +103,84 @@ void SpreadSheet::RemoveRow(int row) {
 		}
 		for (int j = 0; j < Col; j++)
 		{
-			newCells[newRoW][j] = Cells[i][j];
+			Tmp_Cells[index][j] = Cells[i][j];
 		}
-		newRoW++;
+		index++;
 	}
 	Row--;
+	Cells.resize(Row, std::vector<Cell>(Col));
 	for (int i = 0; i < Row; i++)
 	{
 		for (int j = 0; j < Col; j++)
 		{
 
-			Cells[i][j] = newCells[i][j];
+			Cells[i][j] = Tmp_Cells[i][j];
 		}
 	}
-	for (int i = 0; i < Row; i++)
-	{
-		delete[] newCells[i];
-		newCells[i] = nullptr;
-	}
-	delete[] newCells;
-	newCells = nullptr;
 }
 void SpreadSheet::AddColumn(int col) { 
-	int Tmp_Col = col + 1;
-	Cell** Tmp_Cells;
-	Tmp_Cells = new Cell * [Tmp_Col];
-	for (int i = 0; i < Row; ++i) {
-		Tmp_Cells[i] = new Cell[Tmp_Col];
-	}
+	int Tmp_Col = Col + 1;
+	std::vector<std::vector<Cell>> Tmp_Cells;
+	Tmp_Cells.resize(Row, std::vector<Cell>(Tmp_Col));
 	for (int i = 0; i < Row; ++i) {
 		for (int j = 0; j < Tmp_Col; ++j) {
 			Tmp_Cells[i][j].m_value = "";
 			Tmp_Cells[i][j].m_color = color::no_color;
 		}
 	}
-	int k = 0;
 	for (int i = 0; i < Row; ++i) {
-		for (int k = 0; k < col; ++k) {
-			Tmp_Cells[i][k].m_value = Cells[i][k].m_value;
-			Tmp_Cells[i][k].m_color = Cells[i][k].m_color;
-		}
-	}
-	for (int i = 0; i < Row; ++i) {
-		for (int j = col+1; j < Tmp_Col; ++j) {
+		int k = 0;
+		for (int j = 0; j < Tmp_Col; ++j) {
+			if (k >= Col) {
+				std::cout << "Error" << std::endl;
+			}
+			if (j == col) {
+				continue;
+			}
 			Tmp_Cells[i][j].m_value = Cells[i][k].m_value;
 			Tmp_Cells[i][j].m_color = Cells[i][k].m_color;
+			k++;
 		}
-		k++;
 	}
-	for (int i = 0; i < Row; ++i) {
-		delete[] Cells[i];
-	}
-	delete[] Cells;
-	Col++;
-	Cells = new Cell * [Col];
-	for (int i = 0; i < Row; ++i) {
-		Cells[i] = new Cell[Col];
-	}
+	Col = Tmp_Col;
+	this->Cells.resize(Row, std::vector<Cell>(Col));
 	for (int i = 0; i < Row; ++i) {
 		for (int j = 0; j < Col; ++j) {
-			Cells[i][j].m_value = Tmp_Cells[i][j].m_value;
-			Cells[i][j].m_color = Tmp_Cells[i][j].m_color;
+			this->Cells[i][j].m_value = Tmp_Cells[i][j].m_value;
+			this->Cells[i][j].m_color = Tmp_Cells[i][j].m_color;
 		}
 	}
-	for (int i = 0; i < Row; ++i) {
-		delete[] Tmp_Cells[i];
-	}
-	delete[] Tmp_Cells;
 }
 void SpreadSheet::RemoveColumn(int col) { 
-	Cell** newCells = new Cell* [Row];
-	for (int i = 0; i < Row; i++)
-	{
-		newCells[i] = new Cell[Col - 1];
+	int Tmp_Col = Col - 1;
+	std::vector<std::vector<Cell>> Tmp_Cells;
+	Tmp_Cells.resize(Row, std::vector<Cell>(Tmp_Col));
+	for (int i = 0; i < Row; ++i) {
+		for (int j = 0; j < Tmp_Col; ++j) {
+			Tmp_Cells[i][j].m_value = "";
+			Tmp_Cells[i][j].m_color = color::no_color;
+		}
 	}
-
 	for (int i = 0; i < Row; i++)
 	{
-		for (int j = 0, newCol = 0; j < Col; j++)
+		for (int j = 0, index = 0; j < Col; j++)
 		{
 			if (j == col)
 			{
 				continue;
 			}
-			newCells[i][newCol] = Cells[i][j];
-			newCol++;
+			Tmp_Cells[i][index] = Cells[i][j];
+			index++;
 		}
 	}
 	Col--;
-	for (int i = 0; i < Row; i++)
-	{
-		for (int j = 0; j < Col; j++)
-		{
-
-			Cells[i][j] = newCells[i][j];
+	Cells.resize(Row, std::vector<Cell>(Col));
+	for (int i = 0; i < Row; ++i) {
+		for (int j = 0; j < Col; ++j) {
+			this->Cells[i][j].m_value = Tmp_Cells[i][j].m_value;
+			this->Cells[i][j].m_color = Tmp_Cells[i][j].m_color;
 		}
 	}
-	for (int i = 0; i < Row; i++)
-	{
-		delete[] newCells[i];
-		newCells[i] = nullptr;
-	}
-	delete[] newCells;
-	newCells = nullptr;
 }
 void SpreadSheet::SwapRows(int R1, int R2) { 
 	if ((R1 >= Row) || (R2 >= Row)) {
